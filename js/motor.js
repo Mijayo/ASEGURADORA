0d4c6a6b7d20f64a3c193592bdf1a846eb32d947
Diego <48282657+Mijayo@users.noreply.github.com>
2019-11-19 19:38:50 +0100

    test_V3

--- js/motor.js
+++ js/motor.js
@@ -5,17 +5,19 @@ function myFunction() {
     var strUser = e.options[e.selectedIndex].text;
     var y = document.getElementById("year");
     var str = y.options[y.selectedIndex].text;
+    // document.getElementById("demo").style.display = "block";
     modelo(strUser, str);
 }
 
 function modelo(c, y) {
-    document.getElementById("demo").innerHTML = c + " " + y + " " + precio;
+    let secD = document.getElementById("demo");
+    secD.innerHTML = c + " " + y + " " + precio;
 }
 
 let d = new Date();
 let n = d.getFullYear();
 let c = n - 20;
-console.log(c);
+// console.log(c);
 let cont = 0;
 for (c; c <= n; c++) {
     let year = document.getElementById("year");
