86d399a72bfcabda09e111fe16f40b397c814c37
Diego <48282657+Mijayo@users.noreply.github.com>
2019-11-20 18:06:43 +0100

    test_V4

--- js/motor.js
+++ js/motor.js
@@ -1,31 +1,94 @@
-const precio = 600;
+// Quitamos todos los parametros de la URL
+window.history.pushState("object or string", "Title", "/" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split("?")[0]);
 
-function myFunction() {
-    var e = document.getElementById("car");
-    var strUser = e.options[e.selectedIndex].text;
-    var y = document.getElementById("year");
-    var str = y.options[y.selectedIndex].text;
-    // document.getElementById("demo").style.display = "block";
-    modelo(strUser, str);
-}
 
-function modelo(c, y) {
-    let secD = document.getElementById("demo");
-    secD.innerHTML = c + " " + y + " " + precio;
-}
-
+// Obtengo la fecha actual con la funcion Date()
 let d = new Date();
 let n = d.getFullYear();
-let c = n - 20;
-// console.log(c);
+const c = n - 20;
+
 let cont = 0;
-for (c; c <= n; c++) {
+
+for (n; n >= c; n--) {
     let year = document.getElementById("year");
-    // let app = document.write(`<option value="${cont++}">${c}</option>`);
-    // year.appendChild(app);
     let opt = document.createElement('option');
     opt.value = cont++;
-    opt.innerHTML = c;
+    opt.innerHTML = n;
     year.appendChild(opt);
-    // console.log(c);
+}
+
+// Defino las variables constantes del precio base en 600
+const precio = 600;
+const incrementoMarcaAudi = 0.3;
+const incrementoMarcaVolkswagen = 0.2;
+const incrementoMarcaSeat = 0.1;
+const seguroBasico = 0.3;
+const seguroCompleto = 0.5;
+
+
+function yoQueSe() {
+    document.getElementById("spinner").style.display = "block";
+}
+
+
+function myFunction() {
+
+    yoQueSe();
+    // setInterval(yoQueSe, 1000);
+
+    // Marca del coche
+    var e = document.getElementById("car");
+    var strUser = e.options[e.selectedIndex].text;
+
+    // Año del coche
+    var y = document.getElementById("year");
+    var str = y.options[y.selectedIndex].text;
+
+    switch (strUser) {
+        case "Audi":
+            var marca = precio * incrementoMarcaAudi;
+            break;
+        case "Seat":
+            var marca = precio * incrementoMarcaSeat;
+            break;
+        case "Volkswagen":
+            var marca = precio * incrementoMarcaVolkswagen;
+            break;
+    }
+
+    if (document.getElementById('customRadio1').checked) {
+        // Basico 
+        var tipo = precio * seguroBasico;
+
+    } else if (document.getElementById('customRadio2').checked) {
+        // Completo
+        var tipo = precio * seguroCompleto;
+
+    }
+
+    ///// DESCUENTO DEPENDIENDO DEL AÑO /////
+    var dY = document.getElementById("year");
+    let descuentoYear = (dY.options[dY.selectedIndex].value) * 0.3;
+    /////
+
+
+    // Llamo a la funcion calculos pasandole parametros  Para pintar en el HTML
+    calculos(strUser, str, descuentoYear, tipo, marca);
+}
+
+function calculos(c, y, d, t, m) {
+
+    setTimeout(function() {
+
+        document.getElementById("spinner").style.display = "none";
+        document.getElementById("div1").style.display = "block";
+
+        // variables
+        let secD = document.getElementById("demo");
+        let precioFinal = precio + (((t) + (m)) - (d));
+
+        // pinto
+        secD.innerHTML = c + " " + y + " " + precioFinal + "€";
+    }, 2500);
+
 }
